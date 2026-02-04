3ee17d71aff715358dde94f496f4572883a175eb
chenjialong <107867369@qq.com>
2026-02-04 23:49:37 +0800

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  plugins: [
-    '~/app/plugins/useAos.client.ts' // 加上 .client 表示只在客户端运行
-  ],
   css: ["aos/dist/aos.css"],
   build: {
     transpile: (import.meta.client && ["aos"]) || [],
