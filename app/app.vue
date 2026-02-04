b8f7a850f38696866a6c8c83e2b85682245ee559
chenjialong <107867369@qq.com>
2026-02-04 23:32:55 +0800

    add aos

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,7 @@
 <template>
   <div>
-    <AppHeader
-      image-white="https://xuebo-1320531731.cos.ap-shanghai.myqcloud.com/2026/01/30/051f74dc51cc4da183133eb145aa46ae/%E8%93%9D%E6%A9%99%E8%89%B2%E5%AD%97%E6%AF%8D%E5%85%AC%E5%8F%B8Logo%20(12).png"
-      image-black="https://xuebo-1320531731.cos.ap-shanghai.myqcloud.com/2026/01/30/22fd4514f8734d7f95107eff07bc99be/%E8%93%9D%E6%A9%99%E8%89%B2%E5%AD%97%E6%AF%8D%E5%85%AC%E5%8F%B8Logo%20(1).png"
-    />
-
-    <UMain class="min-h-screen">
-      <slot />
-    </UMain>
-
-    <AppFooter />
+    <div data-aos="zoom-out-left">111</div>
+    <div>222</div>
   </div>
 </template>
 <style>
